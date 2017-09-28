066af230aa204e22186a50dca4851aa0e20b0a75
renyubin <renyubin@vacomall.com>
2017-09-28 23:08:53 +0800

    update project

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,21 +1,33 @@
 var admin = require('../dao/adminDao');
 
 module.exports = (app) => {
-    var apiPath = '/' + app.version + '/admins';
-    app.get(apiPath, (req, res, next) => {
-        admin.findAndCount(1, 10)
-            .then((result) => {
-                res.send(result)
-            }, (err) => {
-                res.send(err);
-            })
+  var apiPath = '/' + app.version + '/admins';
+  app.get(apiPath, (req, res, next) => {
+    admin.findAndCount(1, 10)
+      .then((result) => {
+        res.send(result)
+      }, (err) => {
+        res.send(err);
+      })
+  })
+  app.post(apiPath, (req, res, next) => {
+    admin.create({
+      adminName: req.body.username || req.query.adminName,
+      adminPassword: req.body.password || req.query.adminPassword
+    }).then((result) => {
+      res.send(result);
+    }, (err) => {
+      res.send(err);
     })
-    app.post(apiPath, (req, res, next) => {
-        admin.create({
-            adminName: req.query.adminName,
-            adminPassword: req.query.adminPassword
-        }).then((result) => {
-
-        })
+  })
+  app.delete(apiPath + '/:id', (req, res, next) => {
+    admin.remove({
+      adminId: req.params.id
     })
+      .then((result) => {
+        res.send(result);
+      }, (err) => {
+        res.send(err);
+      })
+  })
 }
